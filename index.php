e39c513fa402651dd8b763f0334ed5d1f30a7a79
unknown <dcsbailey21574@gmail.com>
2020-11-13 21:56:10 +0700

    bb

--- index.php
+++ index.php
@@ -1,23 +1,31 @@
-<?php
-# This function reads your DATABASE_URL config var and returns a connection
-# string suitable for pg_connect. Put this in your app.
-function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["DATABASE_URL"]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-}
+<?php 
+        // Create curl resource 
+ /*       $ch = curl_init(); 
+  
+        // set url 
+        //curl_setopt($ch, CURLOPT_URL, "http://10.29.81.199/notify/chk_cogent.php"); 
+        curl_setopt($ch, CURLOPT_URL, "https://script.google.com/macros/s/AKfycbwhjwq4JhNcLqm-9gYeOKh3Glz8v-etLiuylfMHepI/dev?search=AIR"); 
+  
+        // Return the transfer as a string 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+  
+        // $output contains the output string 
+        $output = curl_exec($ch); 
+  
+        echo $output; 
+  
+        // Close curl resource to free up system resources 
+        curl_close($ch);      
+*/
 
-# Here we establish the connection. Yes, that's all.
-$pg_conn = pg_connect(pg_connection_string_from_database_url());
-
-# Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
-
-print "<pre>\n";
-if (!pg_num_rows($result)) {
-  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-} else {
-  print "Tables in your database:\n";
-  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
-}
-print "\n";
-?>
+        $APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzEzi8i6Vo5-Inq_0eFX25MSDx0ragPHgnRrZPOKr0Z0QRPMno/exec';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $APPS_SCRIPT_URL); 
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+        $content = trim(curl_exec($ch));
+        curl_close($ch);    
+        echo $content;      
+        echo "a";
+?> 
