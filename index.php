bd7ef85255655778d3501939ef209b17ce132d7b
unknown <dcsbailey21574@gmail.com>
2020-11-14 13:21:04 +0700

    curl test

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
-        // Create curl resource 
+      /*
+      // Create curl resource 
       $ch = curl_init(); 
   
         // set url 
@@ -16,8 +17,38 @@
   
         // Close curl resource to free up system resources 
         curl_close($ch);      
-
-        echo "aa"
+      */
 ?> 
 
 
+<?php 
+        // Create curl resource 
+ /*       $ch = curl_init(); 
+  
+        // set url 
+        //curl_setopt($ch, CURLOPT_URL, "http://10.29.81.199/notify/chk_cogent.php"); 
+        curl_setopt($ch, CURLOPT_URL, "https://script.google.com/macros/s/AKfycbwhjwq4JhNcLqm-9gYeOKh3Glz8v-etLiuylfMHepI/dev?search=AIR"); 
+  
+        // Return the transfer as a string 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+  
+        // $output contains the output string 
+        $output = curl_exec($ch); 
+  
+        echo $output; 
+  
+        // Close curl resource to free up system resources 
+        curl_close($ch);      
+*/
+
+        $APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzEzi8i6Vo5-Inq_0eFX25MSDx0ragPHgnRrZPOKr0Z0QRPMno/exec';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $APPS_SCRIPT_URL); 
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 20);
+        $content = trim(curl_exec($ch));
+        curl_close($ch);    
+        echo $content;      
+        echo "a";
+?> 
